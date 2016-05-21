469316bee6e286c746bd30f0b300a417cd2663cc
mihajlo.stevanovic <lankiacos@gmail.com>
2016-05-21 22:12:09 +0200

    scroll

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -29,10 +29,58 @@ var BRUTASSO = {
 			$('.page-header').css('background','rgba(0, 80, 120, 0.'+newPosition/10+')');
 		}
 	},
+	scrollTo: function(){
+		$('.navigation a').on('click',function(e){
+			e.preventDefault();
+			var sectionId = $(this).attr('data-section'),
+				section = $('#'+sectionId).offset().top;
+			$('.navigation a').removeClass('active');
+			$(this).addClass('active');
+			$('html,body').animate({
+				scrollTop: section 
+			},'slow');
+		})
+	},
+	scrollSpy: function(){
+		$('.section').each(function(){
+			var sectionPos = $(this).offset().top;
+			if($(window).scrollTop() >= sectionPos + $(window).height()){
+				console.log(this).attr('id');
+			}
+		});
+	},
+	tabs: function(){
+		$('.tab-link').on('click',function(e){
+			e.preventDefault();
+			var currentTab = $(this).attr('data-tab-id'),
+				tabs = $(this).parents('.tabs');
+			$(tabs).find('.tab-link').removeClass('active');
+			$(this).addClass('active');
+			$(tabs).find('.tab-content').removeClass('active');
+			$('#'+currentTab).addClass('active');
+		})
+	},
+	getPartnersDetails(){
+		$('.partner-link').on('click',function(e){
+			e.preventDefault();
+			var currentPartner = $(this).attr('data-partner-id');
+			console.log(currentPartner);
+			$('.partner-link').removeClass('active');
+			$(this).addClass('active');
+			$('.partner-detail').removeClass('active');
+			$('#'+currentPartner).addClass('active');
+		})
+	},
 
 	/* Init the functions */
 	init: function(){
 		this.homeHeight();
+		this.scrollTo();
+		this.tabs();
+		this.getPartnersDetails();
+	},
+	scrollInit: function(){
+		this.scrollSpy();
 	}
 }
 
@@ -43,5 +91,6 @@ $(window).load(function(){
 	BRUTASSO.loader();
 })
 $(window).scroll(function(){
+	BRUTASSO.scrollInit();
 	//BRUTASSO.navOpacity();
 })
