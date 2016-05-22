c709a61c6fe41ecfa4735a7c612ff51e798b5435
Mihajlo Stevanovic <lankiacos@gmail.com>
2016-05-22 13:31:56 +0200

    add fullcalendar & slimscroll

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -30,14 +30,15 @@ var BRUTASSO = {
 		}
 	},
 	scrollTo: function(){
-		$('.navigation a').on('click',function(e){
+		$('.navigation a, .scroll-down').on('click',function(e){
 			e.preventDefault();
 			var sectionId = $(this).attr('data-section'),
-				section = $('#'+sectionId).offset().top;
+				section = $('#'+sectionId).offset().top,
+				headerHeight = $('#header').height();
 			$('.navigation a').removeClass('active');
 			$(this).addClass('active');
 			$('html,body').animate({
-				scrollTop: section 
+				scrollTop: section - headerHeight
 			},'slow');
 		})
 	},
@@ -67,9 +68,58 @@ var BRUTASSO = {
 			console.log(currentPartner);
 			$('.partner-link').removeClass('active');
 			$(this).addClass('active');
-			$('.partner-detail').removeClass('active');
+			$('.partner-detail-wrapper').removeClass('active');
 			$('#'+currentPartner).addClass('active');
 		})
+	},
+	customScrollBar: function(){
+		$('.partner-detail-inner').slimScroll({
+			color: '#fff',
+			height: 600,
+			size: '3px',
+			alwaysVisible: true
+		});
+	},
+	calendar: function(){
+		$('.calendar').fullCalendar({
+			height: 450,
+			header: {
+				left: 'prev',
+				center: 'title',
+				right: 'next'
+		    },
+		});
+	},
+	getFbApi: function(){
+		window.fbAsyncInit = function() {
+			FB.init({
+				appId      : '423617124508884',
+				xfbml      : true,
+				version    : 'v2.4'
+			});
+
+			/* make the API call */
+			FB.api(
+			    "/me/feed",
+			    function (response) {
+			        console.log(response);
+			      if (response && !response.error) {
+			        /* handle the result */
+			        console.log(response);
+			      }
+			    }
+			);
+			
+		};
+
+		(function(d, s, id){
+			var js, fjs = d.getElementsByTagName(s)[0];
+			if (d.getElementById(id)) {return;}
+			js = d.createElement(s); js.id = id;
+			js.src = "//connect.facebook.net/en_US/sdk.js";
+			fjs.parentNode.insertBefore(js, fjs);
+		}(document, 'script', 'facebook-jssdk'));
+
 	},
 
 	/* Init the functions */
@@ -78,9 +128,12 @@ var BRUTASSO = {
 		this.scrollTo();
 		this.tabs();
 		this.getPartnersDetails();
+		this.customScrollBar();
+		this.calendar();
+		this.getFbApi();
 	},
 	scrollInit: function(){
-		this.scrollSpy();
+		//this.scrollSpy();
 	}
 }
 
